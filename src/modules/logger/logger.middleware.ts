31df45ec1787d93ee5b2560d64657ed9e5c4d10f
Evghenii Notarius <dufa456@gmail.com>
2019-08-07 13:16:49 +0200

    fix: remove query obj from logger middleware

--- src/modules/logger/logger.middleware.ts
+++ src/modules/logger/logger.middleware.ts
@@ -7,7 +7,7 @@ import { LoggerService } from "./logger.service"
 export class LoggerMiddleware implements NestMiddleware {
   constructor(private readonly logger: LoggerService) {}
   use(req: Request, res: Response, next: () => void) {
-    this.logger.log(`${req.method} ${req.baseUrl} ${JSON.stringify(req.query)}`)
+    this.logger.log(`${req.method} ${req.baseUrl}`)
     next()
   }
 }
